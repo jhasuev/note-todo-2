7a72e2efcb06e1c4be570ff3e12fc5d38ce78314
Jamal Hasuev <metafra@mail.ru>
2021-08-14 18:46:40 +0300

    remove excess code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,4 @@
 <template>
-
-  <input type="text" class="form-field" placeholder="Текстовое поле">
-  <button class="btn">Кнопка по дефолту</button>
-  <button class="btn  btn--green">Зеленая кнопка</button>
-  <button class="btn  btn--red">Красная кнопка</button>
-
-  <label class="checkbox">
-    <input type="checkbox" class="checkbox__input">
-    <i class="checkbox__icon" />
-  </label>
-
-  <label class="checkbox">
-    <input type="checkbox" class="checkbox__input" checked>
-    <i class="checkbox__icon" />
-  </label>
-
-
   <div class="card">
     <h2 class="card__title">Создание заметки</h2>
     <div class="add">
