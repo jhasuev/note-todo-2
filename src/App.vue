422c25fe50d36c966ab60336ea524c6dc7f2999d
Jamal Hasuev <metafra@mail.ru>
2021-08-14 14:42:52 +0300

    layout for note creating

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,26 @@
   </label>
 
 
+  <div class="card">
+    <h2 class="card__title">Создание заметки</h2>
+    <div class="add">
+      <input type="text" class="form-field" placeholder="Название заметки">
+      <div class="add__todos">
+        <div class="add__todos-list">
+          <div class="add__todos-item">
+            <input type="text" class="form-field  add__todos-field" placeholder="Название пункта">
+          </div>
+          <div class="add__todos-item">
+            <input type="text" class="form-field  add__todos-field" placeholder="Название пункта">
+          </div>
+        </div>
+        <button class="btn add__todos-btn">Добавить пункт</button>
+      </div>
+      <div class="add__submit-wrapper">
+        <button class="btn btn--green">Создать заметку</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -25,6 +45,39 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.card {
+  max-width: 400px;
+  margin: 15px auto;
+  padding: 15px;
+  box-shadow: 0 0 5px rgba(0,0,0,.25);
+
+  &__title {
+    text-align: center;
+    font-size: 20px;
+    margin-bottom: 15px;
+  }
+}
+
+.add {
+  &__todos {
+    margin-top: 20px;
+    border: 1px solid #eee;
+    padding: 15px;
+
+    &-item {
+      margin-bottom: 10px;
+    }
+    &-btn {
+      width: 100%;
+    }
+  }
+  &__submit-wrapper {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+
 
 </style>
