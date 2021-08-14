01c8e95807fee6d0e535bfb6c97f50f44be61234
Jamal Hasuev <metafra@mail.ru>
2021-08-14 12:55:19 +0300

    layout common form elements

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  App.vue
+
+  <input type="text" class="form-field" placeholder="Текстовое поле">
+  <button class="btn">Кнопка по дефолту</button>
+  <button class="btn  btn--green">Зеленая кнопка</button>
+  <button class="btn  btn--red">Красная кнопка</button>
+
+  <label class="checkbox">
+    <input type="checkbox" class="checkbox__input">
+    <i class="checkbox__icon" />
+  </label>
+
+  <label class="checkbox">
+    <input type="checkbox" class="checkbox__input" checked>
+    <i class="checkbox__icon" />
+  </label>
+
+
 </template>
 
 <script>
