d896b8de7fdd9eca092ff70688de8f344f98e1f9
Jamal Hasuev <metafra@mail.ru>
2021-08-15 13:23:10 +0300

    add popup layout

---
+++ src/components/global/popup.vue
@@ -0,0 +1,75 @@
+<template>
+  <transition name="fade" mode="in-out">
+    <div v-if="show" class="popup">
+      <div class="popup__inner">
+        <card class="popup__content" :title=title>
+          <slot />
+        </card>
+      </div>
+      <i class="popup__bg" />
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "Popup",
+  props: {
+    title: { type: String, default: "" },
+    show: { type: Boolean, default: false },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.popup {
+  position: fixed;
+  z-index: 1000;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  &__inner {
+    width: 100%;
+    max-height: 100%;
+    padding: 15px;
+  
+    overflow-y: auto;
+  }
+
+  &__content {
+    background-color: #fff;
+    margin-bottom: 0;
+  }
+
+  &__bg {
+    position: absolute;
+    z-index: -1;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    top: 0;
+
+    background-color: #000;
+    opacity: .75;
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from "./router"
 import store from "./store"
 import Card from "@/components/global/card"
 import Checkbox from "@/components/global/checkbox"
+import Popup from "@/components/global/popup"
 import "./scss/main.scss"
 
 createApp(App)
   .component("Card", Card)
   .component("Checkbox", Checkbox)
+  .component("Popup", Popup)
   .use(router)
   .use(store)
   .mount('#app')
